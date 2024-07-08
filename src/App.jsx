48fb7c9572001bddefb79de6b496fefffbb0aa08
parby8605 <lhj971011@gmail.com>
2024-07-08 17:50:27 +0900

    에러 핸들링

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,36 @@ import "./App.css";
 import { useForm } from "react-hook-form";
 
 function App() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const onSubmit = (data) => console.log(data);
   return (
     <div>
+      {/**handleSubmit(params) params는 submit 전송시 실행되는 콜백함수 */}
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("ID")} placeholder="ID" />
+        <input
+          {...register("ID", { required: true, maxLength: 16 })}
+          placeholder="ID"
+        />
+        <input
+          {...register("mail", { required: "Email Address is required" })}
+          aria-invalid={errors.mail ? "true" : "false"}
+        />
+        {errors.mail && <p role="alert">{errors.mail?.message}</p>}
         <input
           type="password"
           {...register("password")}
           placeholder="password"
+        />
+        <input
+          {...register("EnglishName", {
+            required: true,
+            pattern: /^[A-Za-z]+$/i,
+          })}
+          placeholder="EnglishName"
         />
         <select {...register("gener")}>
           <option value={"male"}>male</option>
