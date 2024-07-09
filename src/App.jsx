d0a6a14cf6b3a56992a27d2243e276ddd76cc56f
parby8605 <lhj971011@gmail.com>
2024-07-09 16:48:19 +0900

    validation 설정

--- src/App.jsx
+++ src/App.jsx
@@ -10,30 +10,44 @@ function App() {
   const onSubmit = (data) => console.log(data);
   return (
     <div>
+      <h2 className="title">React-Hook-Form 예제</h2>
       {/**handleSubmit(params) params는 submit 전송시 실행되는 콜백함수 */}
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
-          {...register("ID", { required: true, maxLength: 16 })}
+          {...register("ID", { required: "ID를 입력해주세요", maxLength: 16 })}
           placeholder="ID"
         />
+        {errors.ID && <p role="alert">{errors.ID?.message}</p>}
         <input
-          {...register("mail", { required: "Email Address is required" })}
+          {...register("mail", { required: "Email을 입력해주세요" })}
           aria-invalid={errors.mail ? "true" : "false"}
+          placeholder="E-mail"
         />
         {errors.mail && <p role="alert">{errors.mail?.message}</p>}
         <input
           type="password"
-          {...register("password")}
-          placeholder="password"
+          {...register("password", {
+            required: "비밀번호를 입력해주세요",
+            minLength: {
+              value: 4,
+              message: "비밀번호는 최소 4자리 이상이여아 합니다",
+            },
+            maxLength: {
+              value: 16,
+              message: "비밀번호는 최대 16자리 이하이여아 합니다",
+            },
+          })}
+          placeholder="password (4~16자리)"
         />
+        {errors.password && <p role="alert">{errors.password?.message}</p>}
         <input
-          {...register("EnglishName", {
-            required: true,
+          {...register("NickName", {
+            required: false,
             pattern: /^[A-Za-z]+$/i,
           })}
-          placeholder="EnglishName"
+          placeholder="NickName - 필수사항이 아닙니다"
         />
-        <select {...register("gener")}>
+        <select {...register("gender")}>
           <option value={"male"}>male</option>
           <option value={"female"}>female</option>
           <option value={"others"}>others</option>
