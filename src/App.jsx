32e957f31339c733189857d6dd6529a5d6544806
parby8605 <lhj971011@gmail.com>
2024-07-10 14:33:50 +0900

    블로그 포스팅 용도로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -11,43 +11,46 @@ function App() {
   return (
     <div>
       <h2 className="title">React-Hook-Form 예제</h2>
-      {/**handleSubmit(params) params는 submit 전송시 실행되는 콜백함수 */}
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
-          {...register("ID", { required: "ID를 입력해주세요", maxLength: 16 })}
+          {...register("ID", { required: "ID를 입력해 주세요" })}
+          aria-invalid={errors.ID ? "true" : "false"}
           placeholder="ID"
         />
-        {errors.ID && <p role="alert">{errors.ID?.message}</p>}
+        {errors.ID && <p role="alert">{errors.ID.message}</p>}
         <input
-          {...register("mail", { required: "Email을 입력해주세요" })}
-          aria-invalid={errors.mail ? "true" : "false"}
+          {...register("email", {
+            required: "이메일을 입력해 주세요.",
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "이메일 형식에 맞지 않습니다.",
+            },
+          })}
+          type="email"
           placeholder="E-mail"
         />
-        {errors.mail && <p role="alert">{errors.mail?.message}</p>}
+        {errors.email && <p role="alert">{errors.email.message}</p>}
         <input
-          type="password"
           {...register("password", {
-            required: "비밀번호를 입력해주세요",
+            required: "비밀번호는 필수 입력입니다.",
             minLength: {
               value: 4,
-              message: "비밀번호는 최소 4자리 이상이여아 합니다",
+              message: "4자리 이상 비밀번호를 사용해주세요.",
             },
             maxLength: {
               value: 16,
-              message: "비밀번호는 최대 16자리 이하이여아 합니다",
+              message: "16자리 이하 비밀번호를 사용해주세요.",
             },
           })}
+          type="password"
           placeholder="password (4~16자리)"
         />
-        {errors.password && <p role="alert">{errors.password?.message}</p>}
+        {errors.password && <p role="alert">{errors.password.message}</p>}
         <input
-          {...register("NickName", {
-            required: false,
-            pattern: /^[A-Za-z]+$/i,
-          })}
+          {...register("NickName")}
           placeholder="NickName - 필수사항이 아닙니다"
         />
-        <select {...register("gender")}>
+        <select {...register("GENDER")}>
           <option value={"male"}>male</option>
           <option value={"female"}>female</option>
           <option value={"others"}>others</option>
